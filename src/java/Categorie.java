f18d4c3218d26d0a5b4e5b0fc7918c42fdd1660d
denis94 <denisca.sturza@gmail.com>
2016-03-01 08:45:27 +0100

    Added files via upload

---
+++ src/java/Categorie.java
@@ -0,0 +1,50 @@
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author FSEVERI\sturza2870
+ */
+@Entity 
+@Table(name="Categorie")  
+
+public class Categorie implements Serializable {
+    @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id_Cat")  
+    private int id;
+    
+    @Column(name="Descrizione",length=20)
+    private String descrizione;
+
+    public Categorie() {
+    }
+
+    public Categorie(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+   
+}
---
+++ src/java/Eventi.java
@@ -0,0 +1,65 @@
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author FSEVERI\sturza2870
+ */
+@Entity 
+@Table(name="Eventi")  
+
+public class Eventi implements Serializable {
+    @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id_Evento")  
+    private int id;
+    
+    @Column(name="Luogo",length=35)
+    private String luogo;
+   
+    @Column(name="Data")
+    private String data;
+
+    public Eventi() {
+    }
+
+    public Eventi(String luogo, String data) {
+        this.luogo = luogo;
+        this.data = data;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLuogo() {
+        return luogo;
+    }
+
+    public void setLuogo(String luogo) {
+        this.luogo = luogo;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+    
+    
+   
+
+ 
+}
