44a3fbcbd5f119c2c16ceb42e1c4ce0214c36cc1
FSEVERI\ramescu2755 <FSEVERI\ramescu2755@lmatbi-26.fseveri.it>
2016-03-01 09:03:29 +0100

    Aggiunta Config Init (Andrei)

---
+++ src/java/main/Init.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import javax.servlet.ServletContext;  
+import javax.servlet.ServletException;  
+import javax.servlet.ServletRegistration.Dynamic;  
+  
+import org.springframework.web.WebApplicationInitializer;  
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;  
+import org.springframework.web.servlet.DispatcherServlet;  
+
+/**
+ *
+ * @author FSEVERI\ramescu2755
+ */
+public class Init implements WebApplicationInitializer{
+
+    @Override
+    public void onStartup(ServletContext sc) throws ServletException {
+        AnnotationConfigWebApplicationContext ctx= new AnnotationConfigWebApplicationContext();
+        ctx.register(Config.class );
+        ctx.setServletContext(sc);
+        Dynamic servlet = sc.addServlet("dispatcher", new DispatcherServlet(ctx));
+        servlet.addMapping("/");
+        servlet.setLoadOnStartup(1);
+    }
+}
