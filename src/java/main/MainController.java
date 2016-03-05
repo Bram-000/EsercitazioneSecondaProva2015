7e8a85f6c5c0f652ee7d7a2981edf1a8521de8f2
FSEVERI\scagnellato3082 <FSEVERI\scagnellato3082@linf2-07.fseveri.it>
2016-03-05 09:39:53 +0100

    add evento di prova

--- src/java/main/MainController.java
+++ src/java/main/MainController.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,14 +10,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class MainController {
     
-    //List<Eventi> eventi = new ArrayList<Eventi>();
+    List<Eventi> eventi = new ArrayList<Eventi>();
 
     public MainController() {
-        
+        eventi.add(new Eventi("padova","2010-10-10"));
     }
     
     @RequestMapping(value="/", method= RequestMethod.GET)
     public String index(ModelMap map){
+        map.put("primo",eventi);
         return "paginaIniziale";
     }
     
