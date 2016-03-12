3a5f0aa388717b78373bf939e786f8403e25969f
FSEVERI\scagnellato3082 <FSEVERI\scagnellato3082@linf2-07.fseveri.it>
2016-03-12 09:30:22 +0100

    prova commenti

--- src/java/main/MainController.java
+++ src/java/main/MainController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class MainController {
     
     List<Eventi> eventi = new ArrayList<Eventi>();
+    List<Commenti> commenti = new ArrayList<Commenti>();
 
     public MainController() {
         eventi.add(new Eventi("padova","2010-10-10"));
@@ -19,20 +20,14 @@ public class MainController {
         eventi.add(new Eventi("venezia","2010-10-10"));
         eventi.add(new Eventi("verona","2010-10-10"));
         eventi.add(new Eventi("padova","2010-10-10"));
-        eventi.add(new Eventi("roma","2010-10-10"));
-        eventi.add(new Eventi("milano","2010-10-10"));
-        eventi.add(new Eventi("venezia","2010-10-10"));
-        eventi.add(new Eventi("verona","2010-10-10"));
-        eventi.add(new Eventi("padova","2010-10-10"));
-        eventi.add(new Eventi("roma","2010-10-10"));
-        eventi.add(new Eventi("milano","2010-10-10"));
-        eventi.add(new Eventi("venezia","2010-10-10"));
-        eventi.add(new Eventi("verona","2010-10-10"));
+        commenti.add(new Commenti("Bell'evento",5));
+        commenti.add(new Commenti("Bell'evento",5));
     }
     
     @RequestMapping(value="/", method= RequestMethod.GET)
     public String index(ModelMap map){
         map.put("eventi",eventi);
+        map.put("commenti",commenti);
         return "paginaIniziale";
     }
     
